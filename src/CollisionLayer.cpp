e56d6df58fa2d0ffe622cbe62deb85bd8d3cbc95
Nerijus <nerijus.vilcinskas@gmail.com>
2022-11-18 22:57:21 +0000

    Simple collision layer made up of vector of bounding boxes

---
+++ src/CollisionLayer.cpp
@@ -0,0 +1,12 @@
+#include "CollisionLayer.hpp"
+#include "CollisionSystem.hpp"
+
+namespace Vigilant {
+    
+    void CollisionLayer::update() {
+    }
+
+    void CollisionLayer::render() {
+        // TODO: Render here bounding boxes optionally
+    }
+}
