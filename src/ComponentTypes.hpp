54735fe81863fe797266dcfd1c14233aebce5639
Nerijus <nerijus.vilcinskas@gmail.com>
2022-11-19 20:06:35 +0000

    Add jumping and sprite flipping

--- src/ComponentTypes.hpp
+++ src/ComponentTypes.hpp
@@ -3,6 +3,7 @@
 
 #include "Vector2D.hpp"
 #include <string>
+#include <unordered_map>
 
 namespace Vigilant {
 
@@ -14,6 +15,8 @@ namespace Vigilant {
     struct Transform {
         float x;
         float y;
+        float scaleX = 1.0f;
+        float scaleY = 1.0f;
         // Vector2D position;
         // TODO: scale, rotation
         
@@ -27,11 +30,35 @@ namespace Vigilant {
         // Vector2D acceleration;
     };
 
+    /*
+     Animation in spritesheet propertries
+
+     1. animations might be in one or multiple files
+     2. each frame is a sprite
+     4. flip must be indicated for some animations like walk left/right
+     5. is animation on the loop or one shot?
+    
+    */
+    struct Animation {
+        std::string filename;
+        int numFrames;
+        int numRow;
+        bool flipped = false;
+    };
+
     struct Sprite {
         int width;
         int height;
         std::string textureID;
-        int numFrames = 0;
+        // std::string fileName;
+        int numFrames = 0; // Num of frames in a row
+        bool flipped = false;
+        std::string animation = "";
+        // std::map<std::string id, int row> animations; flip param is missing
+        // TODO:
+        bool isAnimated = true;
+        int currentFrame = 0;
+        int currentRow = 1; // Row determines type of animation usually
 
         // Different animations
         // sprite h/w vs spritesheet hw
@@ -47,6 +74,7 @@ namespace Vigilant {
     // TODO: rename back to Player once Player class is gone
     struct Playable {
         int health = 100;
+        bool isJumping = false;
     };
 }
 
--- src/InputSystem.hpp
+++ src/InputSystem.hpp
@@ -5,6 +5,7 @@
 #include "Coordinator.hpp"
 #include "ComponentTypes.hpp"
 #include "InputManager.hpp"
+#include "SoundManager.hpp"
 
 namespace Vigilant {
 
@@ -12,6 +13,7 @@ namespace Vigilant {
         public:
             void update(float deltaTime) {
                 for (auto const& entity : entities) {
+                    auto& sprite = TheCoordinator::Instance()->getComponent<Sprite>(entity);
                     auto& transform = TheCoordinator::Instance()->getComponent<Transform>(entity);
                     auto& rigidBody = TheCoordinator::Instance()->getComponent<RigidBody>(entity);
                 
@@ -20,13 +22,24 @@ namespace Vigilant {
                     }
                     if (TheInputManager::Instance()->isKeyDown(SDLK_a)) {
                         transform.x -=  (10.0f * deltaTime);
+                        sprite.flipped = true;
                     }
                     if (TheInputManager::Instance()->isKeyDown(SDLK_s)) {
                         transform.y +=  (10.0f * deltaTime);
                     }
                     if (TheInputManager::Instance()->isKeyDown(SDLK_d)) {
                         transform.x +=  (10.0f * deltaTime);
-                    }  
+                        sprite.flipped = false;
+                    }
+                    if (TheInputManager::Instance()->isKeyPressed(SDLK_SPACE)) {
+                        TheSoundManager::Instance()->playSound("jump", 0);
+                        rigidBody.velocityY = -20.0f;
+                        // transform.x +=  (10.0f * deltaTime);
+                    }
+                    // Alternative movement - follow mouse
+                    // Vector2D target = TheInputManager::Instance()->getMouseCoords();
+                    // m_velocity = target - m_position;
+                    // m_velocity /= 100;
                 }
             }
     };
