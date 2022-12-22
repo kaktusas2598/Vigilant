9f7ee3b9a543b18eaba81569847b203471798193
Nerijus Vilčinskas <nerijus.vilcinskas@gmail.com>
2022-12-22 00:37:40 +0000

    Clean up comments in Particle class

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -10,172 +10,135 @@
 
 namespace Vigilant {
 
-	Particle::Particle() {}
+    Particle::Particle() {}
 
-	void Particle::init(
-		Vector2D pos,
-		float startSpeed, float endSpeed, float angle,
-		double rotSpeed,
-		float startSize,
-		float endSize,
-		Uint32 life,
-		SDL_Rect textureRect,
-		SDL_Color startColor,
-		SDL_Color endColor,
-		SDL_BlendMode blendMode
-		//bool vortexSensitive
-	) {
-		state.live.pos = pos;
-		state.live.startVel.setX(startSpeed * cos(DEG_2_RAD(angle)));
-		state.live.startVel.setY(-startSpeed * sin(DEG_2_RAD(angle)));
-		state.live.endVel.setX(endSpeed * cos(DEG_2_RAD(angle)));
-		state.live.endVel.setY(-endSpeed * sin(DEG_2_RAD(angle)));
-		state.live.startRotSpeed = rotSpeed;
-		state.live.currentRotSpeed = rotSpeed;
+    void Particle::init(
+            Vector2D pos,
+            float startSpeed, float endSpeed, float angle,
+            double rotSpeed,
+            float startSize,
+            float endSize,
+            Uint32 life,
+            SDL_Rect textureRect,
+            SDL_Color startColor,
+            SDL_Color endColor,
+            SDL_BlendMode blendMode
+            //bool vortexSensitive
+            ) {
+        state.live.pos = pos;
+        state.live.startVel.setX(startSpeed * cos(DEG_2_RAD(angle)));
+        state.live.startVel.setY(-startSpeed * sin(DEG_2_RAD(angle)));
+        state.live.endVel.setX(endSpeed * cos(DEG_2_RAD(angle)));
+        state.live.endVel.setY(-endSpeed * sin(DEG_2_RAD(angle)));
+        state.live.startRotSpeed = rotSpeed;
+        state.live.currentRotSpeed = rotSpeed;
 
-		// Life properties
-		this->life = state.live.startLife = life;
-		state.live.currentSize = state.live.startSize = startSize;
-		state.live.endSize = endSize;
-		state.live.t = 0.0f;
+        // Life properties
+        this->life = state.live.startLife = life;
+        state.live.currentSize = state.live.startSize = startSize;
+        state.live.endSize = endSize;
+        state.live.t = 0.0f;
 
-		// Color properties
-		state.live.startColor = startColor;
-		state.live.endColor = endColor;
-		state.live.blendMode = blendMode;
-		state.live.rect = state.live.rectSize = textureRect;
+        // Color properties
+        state.live.startColor = startColor;
+        state.live.endColor = endColor;
+        state.live.blendMode = blendMode;
+        state.live.rect = state.live.rectSize = textureRect;
 
-		// Vortex
-		//state.live.vortexSensitive = vortexSensitive;
-		// Add vortex to the system (optional and only one is allowed)
-		//if (state.live.vortexSensitive)
-			//AddVortex({ 250.0f, 200.0f }, 10.0f, 30.0f);
+        // Vortex
+        //state.live.vortexSensitive = vortexSensitive;
+        // Add vortex to the system (optional and only one is allowed)
+        //if (state.live.vortexSensitive)
+        //AddVortex({ 250.0f, 200.0f }, 10.0f, 30.0f);
 
-	}
+    }
 
-	void Particle::update(float deltaTime) {
-		// Vortex control
-		//if (!state.live.vortexSensitive && vortex.scale != 0 && vortex.speed != 0)
-			//AddVortex({ 0.0f, 0.0f }, 0.0f, 0.0f);
+    void Particle::update(float deltaTime) {
+        // Vortex control
+        //if (!state.live.vortexSensitive && vortex.scale != 0 && vortex.speed != 0)
+        //AddVortex({ 0.0f, 0.0f }, 0.0f, 0.0f);
 
-		// Age ratio is used to interpolate between particle properties
-		state.live.ageRatio = (float)life / (float)state.live.startLife;
+        // Age ratio is used to interpolate between particle properties
+        state.live.ageRatio = (float)life / (float)state.live.startLife;
 
-		// Particle size interpolation
-		state.live.currentSize = interpolateRange(state.live.startSize, state.live.t, state.live.endSize);
+        // Particle size interpolation
+        state.live.currentSize = interpolateRange(state.live.startSize, state.live.t, state.live.endSize);
 
-		// Particle speed interpolation
-		state.live.currentVel.setX(interpolateRange(state.live.startVel.getX(), state.live.t, state.live.endVel.getX()));
-		state.live.currentVel.setY(interpolateRange(state.live.startVel.getY(), state.live.t, state.live.endVel.getY()));
+        // Particle speed interpolation
+        state.live.currentVel.setX(interpolateRange(state.live.startVel.getX(), state.live.t, state.live.endVel.getX()));
+        state.live.currentVel.setY(interpolateRange(state.live.startVel.getY(), state.live.t, state.live.endVel.getY()));
 
-		// Assign new size to particle rect
-		state.live.rectSize.w = state.live.rectSize.h = state.live.currentSize;
+        // Assign new size to particle rect
+        state.live.rectSize.w = state.live.rectSize.h = state.live.currentSize;
 
-		calculateParticlePosition(deltaTime);
+        calculateParticlePosition(deltaTime);
 
-		// Decrementing particle life
-		life--;
-	}
+        // Decrementing particle life
+        life--;
+    }
 
-	void Particle::render() {
+    void Particle::render() {
 
-		// Calculations to determine the current center of particle texture
-		SDL_Rect tmpRect = { (int)state.live.startSize, (int)state.live.startSize };
-		float centerX = state.live.pos.getX() + ((tmpRect.w - state.live.rectSize.w) / 2.0f);
-		float centerY = state.live.pos.getY() + ((tmpRect.h - state.live.rectSize.h) / 2.0f);
+        // Calculations to determine the current center of particle texture
+        SDL_Rect tmpRect = { (int)state.live.startSize, (int)state.live.startSize };
+        float centerX = state.live.pos.getX() + ((tmpRect.w - state.live.rectSize.w) / 2.0f);
+        float centerY = state.live.pos.getY() + ((tmpRect.h - state.live.rectSize.h) / 2.0f);
 
-		// Color interpolation, only if the particle has enough life
-		SDL_Color resColor;
+        // Color interpolation, only if the particle has enough life
+        SDL_Color resColor;
 
-		if (state.live.startLife > MIN_LIFE_TO_INTERPOLATE)
-			resColor = rgbInterpolation(state.live.startColor, state.live.t, state.live.endColor);
+        if (state.live.startLife > MIN_LIFE_TO_INTERPOLATE)
+            resColor = rgbInterpolation(state.live.startColor, state.live.t, state.live.endColor);
 
-		// Refactor these into rendering, also particleState needs texture id
-		SDL_Texture* texture = TheTextureManager::Instance()->getTexture("particle");
-		if (SDL_SetTextureColorMod(texture, resColor.r, resColor.g, resColor.b) != 0)
-			Logger::Instance()->error(SDL_GetError());
-		if (SDL_SetTextureAlphaMod(texture, resColor.a) != 0)
-			Logger::Instance()->error(SDL_GetError());
-		if (SDL_SetTextureBlendMode(texture, state.live.blendMode) != 0)
-			Logger::Instance()->error(SDL_GetError());
+        // Refactor these into rendering, also particleState needs texture id
+        SDL_Texture* texture = TheTextureManager::Instance()->getTexture("particle");
+        if (SDL_SetTextureColorMod(texture, resColor.r, resColor.g, resColor.b) != 0)
+            Logger::Instance()->error(SDL_GetError());
+        if (SDL_SetTextureAlphaMod(texture, resColor.a) != 0)
+            Logger::Instance()->error(SDL_GetError());
+        if (SDL_SetTextureBlendMode(texture, state.live.blendMode) != 0)
+            Logger::Instance()->error(SDL_GetError());
 
-		//TheTextureManager::draw(std::string id, int x, int y, int width, int height, double angle);
-		TheTextureManager::Instance()->draw("particle", (int)centerX, (int)centerY, state.live.rectSize.w, state.live.rectSize.h, state.live.currentRotSpeed);
-		// Blitting particle on screen
-		//BlitParticle(App->psystem->GetParticleAtlas(), (int)centerX, (int)centerY, &state.live.pRect, &state.live.rectSize, resColor, state.live.blendMode, 1.0f, state.live.currentRotSpeed);
-		//BlitParticle(SDL_Texture* texture, int x, int y, const SDL_Rect* section, const SDL_Rect* rectSize, SDL_Color color, SDL_BlendMode blendMode, float speed, double angle)
-//{
-	//SDL_Rect rect;
-	//rect.x = (int)(camera.x * speed) + x * scale;
-	//rect.y = (int)(camera.y * speed) + y * scale;
+        TheTextureManager::Instance()->draw("particle", (int)centerX, (int)centerY, state.live.rectSize.w, state.live.rectSize.h, state.live.currentRotSpeed);
 
-	//if (rectSize != NULL)
-	//{
-		//rect.w = rectSize->w;
-		//rect.h = rectSize->h;
-	//}
-	//else if(section != NULL)
-	//{
-		//rect.w = section->w;
-		//rect.h = section->h;
-	//}
-	//else
-		//SDL_QueryTexture(texture, NULL, NULL, &rect.w, &rect.h);
 
-	//int px = rect.w / 2;
-	//int py = rect.h / 2;
+        // Calculating new rotation according to rotation speed
+        state.live.currentRotSpeed += state.live.startRotSpeed;
 
-	//rect.w *= scale;
-	//rect.h *= scale;
+        // Time step increment to interpolate colors
+        state.live.t += (1.0f / (float)state.live.startLife);
 
-// Color mode setting here
+        if (state.live.t >= 1.0f)
+            state.live.t = 0.0f;
+    }
 
-	//if (SDL_RenderCopyEx(renderer, texture, section, &rect, angle, NULL, SDL_FLIP_NONE) != 0)
-	//{
-		//LOG("Cannot blit to screen. SDL_RenderCopy error: %s", SDL_GetError());
-		//ret = false;
-	//}
+    SDL_Color Particle::rgbInterpolation(SDL_Color startColor, float timeStep, SDL_Color endColor) {
+        SDL_Color finalColor;
 
-	//return ret;
-//}
+        finalColor.r = startColor.r + (endColor.r - startColor.r) * timeStep;
+        finalColor.g = startColor.g + (endColor.g - startColor.g) * timeStep;
+        finalColor.b = startColor.b + (endColor.b - startColor.b) * timeStep;
+        finalColor.a = startColor.a + (endColor.a - startColor.a) * timeStep;
 
-		// Calculating new rotation according to rotation speed
-		state.live.currentRotSpeed += state.live.startRotSpeed;
+        return finalColor;
+    }
 
-		// Time step increment to interpolate colors
-		state.live.t += (1.0f / (float)state.live.startLife);
+    float Particle::interpolateRange(float min, float timeStep, float max) {
+        return min + (max - min) * timeStep;
+    }
 
-		if (state.live.t >= 1.0f)
-			state.live.t = 0.0f;
-	}
+    void Particle::calculateParticlePosition(float deltaTime) {
+        //float dx = state.live.pos.x - vortex.pos.x;
+        //float dy = state.live.pos.y - vortex.pos.y;
+        //float vx = -dy * vortex.speed;
+        //float vy = dx * vortex.speed;
+        //float factor = 1.0f / (1.0f + (dx * dx + dy * dy) / vortex.scale);
 
-	SDL_Color Particle::rgbInterpolation(SDL_Color startColor, float timeStep, SDL_Color endColor) {
-		SDL_Color finalColor;
+        //state.live.pos.x += (vx - state.live.currentVel.x) * factor + state.live.currentVel.x * deltaTime;
+        //state.live.pos.y += (vy - state.live.currentVel.y) * factor + state.live.currentVel.y * deltaTime;
 
-		finalColor.r = startColor.r + (endColor.r - startColor.r) * timeStep;
-		finalColor.g = startColor.g + (endColor.g - startColor.g) * timeStep;
-		finalColor.b = startColor.b + (endColor.b - startColor.b) * timeStep;
-		finalColor.a = startColor.a + (endColor.a - startColor.a) * timeStep;
-
-		return finalColor;
-	}
-
-	float Particle::interpolateRange(float min, float timeStep, float max) {
-		return min + (max - min) * timeStep;
-	}
-
-	void Particle::calculateParticlePosition(float deltaTime) {
-		//float dx = state.live.pos.x - vortex.pos.x;
-		//float dy = state.live.pos.y - vortex.pos.y;
-		//float vx = -dy * vortex.speed;
-		//float vy = dx * vortex.speed;
-		//float factor = 1.0f / (1.0f + (dx * dx + dy * dy) / vortex.scale);
-
-		//state.live.pos.x += (vx - state.live.currentVel.x) * factor + state.live.currentVel.x * deltaTime;
-		//state.live.pos.y += (vy - state.live.currentVel.y) * factor + state.live.currentVel.y * deltaTime;
-
-		state.live.pos.setX(state.live.pos.getX() + state.live.currentVel.getX() + state.live.currentVel.getX() * deltaTime);
-		state.live.pos.setY(state.live.pos.getY() + state.live.currentVel.getY() + state.live.currentVel.getY() * deltaTime);
-	}
+        state.live.pos.setX(state.live.pos.getX() + state.live.currentVel.getX() + state.live.currentVel.getX() * deltaTime);
+        state.live.pos.setY(state.live.pos.getY() + state.live.currentVel.getY() + state.live.currentVel.getY() * deltaTime);
+    }
 
 }
--- src/Particle.hpp
+++ src/Particle.hpp
@@ -9,80 +9,80 @@
 
 namespace Vigilant {
 
-	class Particle {
-		public:
-			Particle();
+    class Particle {
+        public:
+            Particle();
 
-			void init(
-				Vector2D pos,
-				float startSpeed, float endSpeed, float angle,
-				double rotSpeed,
-				float startSize,
-				float endSize,
-				Uint32 life,
-				SDL_Rect textureRect,
-				SDL_Color startColor,
-				SDL_Color endColor,
-				SDL_BlendMode blendMode
-				//bool vortexSensitive
-			);
+            void init(
+                    Vector2D pos,
+                    float startSpeed, float endSpeed, float angle,
+                    double rotSpeed,
+                    float startSize,
+                    float endSize,
+                    Uint32 life,
+                    SDL_Rect textureRect,
+                    SDL_Color startColor,
+                    SDL_Color endColor,
+                    SDL_BlendMode blendMode
+                    //bool vortexSensitive
+                    );
 
 
-			void update(float deltaTime);
-			void render();
+            void update(float deltaTime);
+            void render();
 
-			Particle* getNext() const { return state.next; }
-			void setNext(Particle* next) { state.next = next; }
+            Particle* getNext() const { return state.next; }
+            void setNext(Particle* next) { state.next = next; }
 
-			bool isAlive() { return life > 0; }
+            bool isAlive() { return life > 0; }
 
-			SDL_Color rgbInterpolation(SDL_Color startColor, float timeStep, SDL_Color endColor);
-			float interpolateRange(float min, float timeStep, float max);
+            SDL_Color rgbInterpolation(SDL_Color startColor, float timeStep, SDL_Color endColor);
+            float interpolateRange(float min, float timeStep, float max);
 
-			void calculateParticlePosition(float deltaTime);
+            void calculateParticlePosition(float deltaTime);
 
-		private:
-			//TODO: vortex?
+        private:
+            //TODO: vortex?
 
-			/*  This is the only variable we care about no matter if
-			 *  the particle is alive or dead */
-			Uint32 life = 0;
+            /*  This is the only variable we care about no matter if
+             *  the particle is alive or dead */
+            Uint32 life = 0;
 
-			union ParticleInfo {
-				/* This struct holds the state of the particle when
-				 * it's being update (it's still alive).*/
-				struct ParticleState
-				{
-					Uint32 startLife;
-					Vector2D pos;
-					Vector2D startVel;
-					Vector2D endVel;
-					Vector2D currentVel;
-					float currentSize, startSize, endSize;
-					float ageRatio;
-					float angle;
-					double startRotSpeed;
-					double currentRotSpeed;
-					SDL_Rect rect;
-					SDL_Rect rectSize;
-					SDL_Color startColor;
-					SDL_Color endColor;
-					SDL_BlendMode blendMode;
-					//bool vortexSensitive;
-					float t;
+            union ParticleInfo {
+                /* This struct holds the state of the particle when
+                 * it's being update (it's still alive).*/
+                struct ParticleState
+                {
+                    Uint32 startLife;
+                    Vector2D pos;
+                    Vector2D startVel;
+                    Vector2D endVel;
+                    Vector2D currentVel;
+                    float currentSize, startSize, endSize;
+                    float ageRatio;
+                    float angle;
+                    double startRotSpeed;
+                    double currentRotSpeed;
+                    SDL_Rect rect;
+                    SDL_Rect rectSize;
+                    SDL_Color startColor;
+                    SDL_Color endColor;
+                    SDL_BlendMode blendMode;
+                    //bool vortexSensitive;
+                    float t;
 
-					ParticleState() {}
-				} live;
+                    ParticleState() {}
+                } live;
 
-				/* If the particle is dead, then the 'next' member comes
-				 * into play and the struct it's not used. This pointer
-				 * called 'next' holds a pointer to the next available
-				 * particle after this one. */
-				Particle* next;
+                /* If the particle is dead, then the 'next' member comes
+                 * into play and the struct it's not used. This pointer
+                 * called 'next' holds a pointer to the next available
+                 * particle after this one. */
+                Particle* next;
 
-				ParticleInfo() {}
-			} state;
-	};
+                ParticleInfo() {}
+            } state;
+    };
 }
 
 #endif // __Particle__
